1b5efb82ed4a9b1aa5257c054318f972d5597e95
NavneethReejish <73111455+NavneethReejish@users.noreply.github.com>
2021-01-04 13:43:03 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var database, canvas;
+var backgroundImg;
+var gameState = 0;
+var playerCount;
+var form, player, game;
+
+
+
+function setup(){
+  database = firebase.database();
+  createCanvas(400,400);
+  
+  game = new Game();
+  game.getState();
+  game.start();
+
+ 
+}
+
+function draw(){
+  background("white");
+  
+    
+}
+
